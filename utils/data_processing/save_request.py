04ca65fd607ee751bb0fbeed77809dbd8395c9d2
llevv <llevvt@yandex.ru>
2023-02-08 16:28:25 +0600

    Fixed bug in save_request.py

--- utils/data_processing/save_request.py
+++ utils/data_processing/save_request.py
@@ -9,21 +9,25 @@ def save_request(request_dict, user_id):
             cur = database.cursor()
             cur.execute("SELECT * FROM history WHERE userid='{}';".format(str(user_id)))
             results = cur.fetchall()
-            if len(results) <= 1:
-                add_new_object(request_dict, user_id)
+            if len(results) <= 9:
+                add_new_object(user_id, request_dict=request_dict, )
             else:
                 cur.execute("DELETE FROM history WHERE userid='{}'".format(user_id))
                 database.commit()
-                add_new_object(request_dict, user_id)
+                result = results[1:]
+                for i_request in result:
+                    add_new_object(user_id=user_id, new_tuple=i_request)
+                add_new_object(user_id, request_dict = request_dict)
     else:
-        add_new_object(request_dict, user_id)
+        add_new_object(user_id, request_dict = request_dict)
 
 
-def add_new_object(request_dict, user_id):
-    new_tuple = (
-        user_id, request_dict['url'], request_dict['name'], request_dict['city'],
-        str(request_dict['rating']), str(request_dict['price'])
-    )
+def add_new_object(user_id, new_tuple=None, request_dict=None):
+    if request_dict:
+        new_tuple = (
+            user_id, request_dict['url'], request_dict['name'], request_dict['city'],
+            str(request_dict['rating']), str(request_dict['price'])
+        )
 
     with sqlite3.connect(r'{}'.format(history_path)) as database:
         cur = database.cursor()
@@ -38,5 +42,3 @@ def add_new_object(request_dict, user_id):
         database.commit()
         cur.execute("INSERT INTO history VALUES(?,?,?,?,?,?);", new_tuple)
         database.commit()
-
-
